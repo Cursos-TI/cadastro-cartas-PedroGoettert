e59b2167c546d4283ef11c1f67cc4d22558f40a4
Pedro de Souza Goettert <pedrogoettert@hotmail.com>
2026-01-15 11:40:43 -0300

    define card variables

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,13 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  // Estou usando variaveis em inglês como parte do estudo também.
+  // Nas pesquisas vi que na linguagem C é mais comum usar nomes de variáveis em snake case
+
+  int card_id, population, number_of_tourist_attractions;
+  char city_name[50], state;
+  float area, gdp;
+
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade

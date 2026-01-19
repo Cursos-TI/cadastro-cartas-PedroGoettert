ed52d4a0241df2dbb3d34bb3b352fba3a477506b
Pedro de Souza Goettert <pedrogoettert@hotmail.com>
2026-01-19 09:48:10 -0300

    feat: calculate population density and GDP per capita in reais

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -13,12 +13,17 @@ int main()
   // Nas pesquisas vi que na linguagem C é mais comum usar nomes de variáveis em snake case
 
   int card_id1, population1, number_of_tourist_attractions1;
+  float population_density1, gdp_per_capita1;
   char city_name1[50], state1;
   float area1, gdp1;
 
   int card_id2, population2, number_of_tourist_attractions2;
+  float population_density2, gdp_per_capita2;
   char city_name2[50], state2;
   float area2, gdp2;
+
+  // Define a variável billion para conversão de bilhões para unidades
+  float billion = 1e9f;
 
   // Área para entrada de dados
 
@@ -48,6 +53,13 @@ int main()
   printf("Numero de pontos turisticos ");
   scanf("%d", &number_of_tourist_attractions1);
 
+  // Faz a divisão da população pela área para obter a densidade populacional por hab/km²
+  population_density1 = population1 / area1;
+
+  gdp_per_capita1 = (gdp1 * billion) / population1; // Multiplico por 1000 para converter de bilhões para milhões
+
+  printf("---------------------------------------------------\n");
+
   printf("\n Insira os dados da segunda carta \n");
 
   printf("Estado da carta: "); // No problema o estádo é de A até H (Cada uma das letras representa um estado do pais)
@@ -74,27 +86,40 @@ int main()
   printf("Numero de pontos turisticos ");
   scanf("%d", &number_of_tourist_attractions2);
 
+  // Faz a divisão da população pela área para obter a densidade populacional por hab/km²
+  population_density2 = population2 / area2;
+
+  gdp_per_capita2 = (gdp2 * 1e9f) / population2; // Multiplico por 1000 para converter de bilhões para milhões
+
+  printf("---------------------------------------------------\n");
+
   // Área para exibição dos dados da cidade
 
   printf("\nDados da carta 1: \n");
 
   printf("Estado: %c \n", state1);
   printf("Cidade: %s \n", city_name1);
-  printf("Código da carta: %c%d \n", state1, card_id1);
+  printf("Código da carta: %c%02d \n", state1, card_id1);
   printf("População: %d \n", population1);
   printf("Área: %.2f km² \n", area1);
   printf("PIB: %.2f bilhões \n", gdp1);
   printf("Pontos Turísticos: %d \n", number_of_tourist_attractions1);
+  printf("Densidade populacional: %.2f hab/km² \n", population_density1);
+  printf("PIB per capita: %.2f reais \n", gdp_per_capita1);
+
+  printf("---------------------------------------------------\n");
 
   printf("\nDados da carta 2: \n");
 
   printf("Estado: %c \n", state2);
   printf("Cidade: %s \n", city_name2);
-  printf("Código da carta: %c%d \n", state2, card_id2);
+  printf("Código da carta: %c%02d \n", state2, card_id2);
   printf("População: %d \n", population2);
   printf("Área: %.2f km² \n", area2);
   printf("PIB: %.2f bilhões \n", gdp2);
   printf("Pontos Turísticos: %d \n", number_of_tourist_attractions2);
+  printf("Densidade populacional: %.2f hab/km² \n", population_density2);
+  printf("PIB per capita: %.2f reais \n", gdp_per_capita2);
 
   return 0;
 }
